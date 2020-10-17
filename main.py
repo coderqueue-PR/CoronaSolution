24297b9cccf2e076b61c7e83fd2c8f20e8bee2de
PR-coderqueue <72966339+coderqueue-PR@users.noreply.github.com>
2020-10-17 12:27:18 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask , render_template,request
+app = Flask(__name__)
+import pickle
+
+
+
+# open a file, where you stored the pickled data
+file = open('model.pkl', 'rb')
+clf = pickle.load(file)
+file.close()
+
+@app.route('/' , methods = ["GET" , "POST"])
+def hello_world():
+     if request.method == "POST":
+         
+          myDict = request.form
+          fever = int(myDict['fever'])
+          age = int(myDict['age'])
+          Pain = int(myDict['Pain'])
+          runnyNose = int(myDict['runnyNose'])
+          diffBreath = int(myDict['diffbreath'])
+     
+          # code for reference
+          inputFeatures = [fever , Pain, age,  runnyNose, diffBreath]
+          infProb = clf.predict_proba([inputFeatures])[0][1]
+          print(infProb)
+        
+          return render_template('show.html' , inf=round(infProb*100))
+     return render_template('index.html')
+          #return 'Hello,World!'  + str(infProb)
+
+
+
+if __name__ == "__main__":
+     app.run(debug=True)
+       
+    
+        
+    
